2c693a86e5f919fc9596d3d1e42ddd973bd59118
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-05-11 09:59:37 +0700

    feat: prevent window size too small

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "App",
         eframe::NativeOptions {
-            viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 540.0]),
+            viewport: egui::ViewportBuilder::default()
+                .with_inner_size([800.0, 540.0])
+                .with_min_inner_size([600.0, 400.0]),
             ..Default::default()
         },
         Box::new(|cc| Ok(Box::new(App::new(cc)))),
