2c4e62b64d0508fdcfa4fb23280d16b4f2d17e0a
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2025-11-22 15:02:37 +0700

    feat: track list keyboard controls (#25)

--- src/ui/track_list.rs
+++ src/ui/track_list.rs
@@ -1,11 +1,13 @@
 use eframe::egui;
 use eframe::egui::include_image;
+use egui_extras::{Column, TableBuilder};
 
 use crate::{player::MusicPlayer, track::Track};
 
 #[derive(Default, Clone)]
 struct State {
     search: String,
+    selected_index: Option<usize>,
 }
 
 impl State {
@@ -30,46 +32,90 @@ impl<'a> TrackList<'a> {
 
 impl egui::Widget for TrackList<'_> {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        use egui_extras::{Column, TableBuilder};
-
-        let ctx = ui.ctx().clone();
-
         let id = ui.next_auto_id();
-        let mut state = State::load(&ctx, id).unwrap_or_default();
+        let mut state = State::load(ui.ctx(), id).unwrap_or_default();
 
         ui.vertical(|ui| {
-            let search_box_response = ui.add_sized(
-                [ui.available_width(), 30.0],
-                egui::TextEdit::singleline(&mut state.search)
-                    .vertical_align(egui::Align::Center)
-                    .hint_text("Search"),
-            );
-
             let mut widget_focused = false;
-
-            ui.ctx().memory(|memory| {
+            ui.memory(|memory| {
                 if memory.focused().is_some() {
                     widget_focused = true;
                 }
             });
 
-            if !widget_focused {
-                let mut search_box_request_focus = false;
-
-                ui.ctx().input_mut(|input_state| {
+            let mut search_request = false;
+            let mut enter_pressed = false;
+            let mut select_changed = false;
+            ui.input_mut(|input_state| {
+                if !widget_focused {
                     if input_state.consume_key(egui::Modifiers::CTRL, egui::Key::F) {
-                        search_box_request_focus = true;
+                        search_request = true;
                     }
-                });
-
-                if search_box_request_focus {
-                    search_box_response.request_focus();
+                    if input_state.consume_key(egui::Modifiers::NONE, egui::Key::Escape) {
+                        state.selected_index = None;
+                    }
                 }
+                if input_state.consume_key(egui::Modifiers::NONE, egui::Key::Enter) {
+                    enter_pressed = true;
+                }
+                if input_state.consume_key(egui::Modifiers::NONE, egui::Key::ArrowUp) {
+                    if let Some(selected) = state.selected_index.as_mut() {
+                        *selected = selected.saturating_sub(1);
+                    } else {
+                        state.selected_index = Some(usize::MAX);
+                    }
+                    select_changed = true;
+                }
+                if input_state.consume_key(egui::Modifiers::NONE, egui::Key::ArrowDown) {
+                    if let Some(selected) = state.selected_index.as_mut() {
+                        *selected = selected.saturating_add(1);
+                    } else {
+                        state.selected_index = Some(usize::MIN);
+                    }
+                    select_changed = true;
+                }
+            });
+
+            let search_input = ui.add_sized(
+                [ui.available_width(), 30.0],
+                egui::TextEdit::singleline(&mut state.search)
+                    .vertical_align(egui::Align::Center)
+                    .hint_text("Search"),
+            );
+            if search_input.changed() {
+                state.selected_index = None;
+            }
+            if search_request {
+                search_input.request_focus();
             }
 
-            let width = ui.available_width();
+            let tracks = self
+                .player
+                .playlist()
+                .tracks()
+                .iter()
+                .enumerate()
+                .filter(|item| {
+                    if state.search.is_empty() {
+                        return true;
+                    }
+                    format!(
+                        "{} {} {}",
+                        item.1.album.as_deref().unwrap_or(""),
+                        item.1.title.as_deref().unwrap_or(""),
+                        item.1.artist.as_deref().unwrap_or(""),
+                    )
+                    .to_ascii_lowercase()
+                    .trim()
+                    .contains(&state.search.to_ascii_lowercase())
+                })
+                .collect::<Vec<(usize, &Track)>>();
 
-            TableBuilder::new(ui)
+            // NOTE: To avoid track clone, store to be act index and handle later.
+            let mut action_index: Option<usize> = None;
+
+            let width = ui.available_width();
+            let mut table = TableBuilder::new(ui)
                 .sense(egui::Sense::click())
                 .striped(true)
                 .resizable(true)
@@ -87,7 +133,25 @@ impl egui::Widget for TrackList<'_> {
                         .clip(true),
                 )
                 .column(Column::remainder().clip(true))
-                .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                .cell_layout(egui::Layout::left_to_right(egui::Align::Center));
+
+            let total = tracks.len();
+
+            if !state.search.is_empty() && total == 1 {
+                state.selected_index = Some(0);
+            }
+            if let Some(index) = state.selected_index.as_mut() {
+                *index = index.to_owned().clamp(0, total.saturating_sub(1));
+
+                if enter_pressed {
+                    action_index = tracks.get(*index).map(|item| item.0);
+                }
+                if select_changed {
+                    table = table.scroll_to_row(*index, None);
+                }
+            }
+
+            table
                 .header(32.0, |mut header| {
                     header.col(|ui| {
                         ui.centered_and_justified(|ui| {
@@ -112,33 +176,13 @@ impl egui::Widget for TrackList<'_> {
                 .body(|mut body| {
                     body.ui_mut().style_mut().interaction.selectable_labels = false;
 
-                    let tracks = self
-                        .player
-                        .playlist()
-                        .tracks()
-                        .iter()
-                        .enumerate()
-                        .filter(|(_index, item)| {
-                            if state.search.is_empty() {
-                                return true;
-                            }
-                            format!(
-                                "{} {} {}",
-                                item.album.as_deref().unwrap_or(""),
-                                item.title.as_deref().unwrap_or(""),
-                                item.artist.as_deref().unwrap_or(""),
-                            )
-                            .to_ascii_lowercase()
-                            .trim()
-                            .contains(&state.search.to_ascii_lowercase())
-                        })
-                        .collect::<Vec<(usize, &Track)>>();
+                    body.rows(24.0, total, |mut row| {
+                        let row_index = row.index();
+                        let (playlist_index, playlist_track) = tracks[row_index];
 
-                    // NOTE: To avoid tracks clone, store double clicked index and handle later.
-                    let mut double_clicked_index: Option<usize> = None;
-
-                    body.rows(24.0, tracks.len(), |mut row| {
-                        let (index, item) = tracks[row.index()];
+                        if state.selected_index.is_some_and(|index| index == row_index) {
+                            row.set_selected(true);
+                        }
 
                         row.col(|ui| {
                             ui.centered_and_justified(|ui| {
@@ -146,7 +190,7 @@ impl egui::Widget for TrackList<'_> {
                                     && self
                                         .player
                                         .current_track()
-                                        .is_some_and(|track| track.eq(item))
+                                        .is_some_and(|track| track.eq(playlist_track))
                                 {
                                     ui.add(
                                         egui::Image::new(if self.player.is_paused() {
@@ -160,14 +204,14 @@ impl egui::Widget for TrackList<'_> {
                             });
                         });
                         row.col(|ui| {
-                            ui.label(item.album.as_deref().unwrap_or("-"));
+                            ui.label(playlist_track.album.as_deref().unwrap_or("-"));
                         });
                         row.col(|ui| {
                             ui.with_layout(
                                 egui::Layout::right_to_left(egui::Align::Center),
                                 |ui| {
-                                    let disc = item.disc.as_deref().unwrap_or_default();
-                                    let track = item.track.as_deref().unwrap_or_default();
+                                    let disc = playlist_track.disc.as_deref().unwrap_or_default();
+                                    let track = playlist_track.track.as_deref().unwrap_or_default();
 
                                     match (disc.is_empty(), track.is_empty()) {
                                         (false, false) => {
@@ -182,26 +226,29 @@ impl egui::Widget for TrackList<'_> {
                             );
                         });
                         row.col(|ui| {
-                            ui.label(item.title.as_deref().unwrap_or("-"));
+                            ui.label(playlist_track.title.as_deref().unwrap_or("-"));
                         });
                         row.col(|ui| {
-                            ui.label(item.artist.as_deref().unwrap_or("-"));
+                            ui.label(playlist_track.artist.as_deref().unwrap_or("-"));
                         });
 
+                        if row.response().clicked() {
+                            state.selected_index = Some(row_index);
+                        }
                         if row.response().double_clicked() {
-                            double_clicked_index = Some(index);
+                            action_index = Some(playlist_index);
                         }
                     });
-
-                    if let Some(index) = double_clicked_index {
-                        let track = self.player.playlist().tracks()[index].to_owned();
-
-                        self.player.playlist_mut().select_track(index.to_owned());
-                        self.player.play_track(track);
-                    }
                 });
 
-            state.store(&ctx, id);
+            if let Some(index) = action_index {
+                self.player.playlist_mut().select_track(index);
+                if let Some(track) = self.player.playlist().current_track() {
+                    self.player.play_track(track.to_owned());
+                }
+            }
+
+            state.store(ui.ctx(), id);
         })
         .response
     }
